a9b675508daed9ec268e54dc9f14aec95d010f04
timmyFrederiksen <timmyfrederiksen@gmail.com>
2022-04-28 22:28:51 -0500

    nps utils test v.1
    initial commit

---
+++ src/test/java/bearcation/utils/NPSUtilsTests.java
@@ -0,0 +1,32 @@
+package bearcation.utils;
+
+import bearcation.model.requests.Activity;
+import bearcation.model.requests.NationalPark;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Nested
+@DisplayName("NPS Utils Tests")
+public class NPSUtilsTests {
+
+//    @Test
+//    @DisplayName("Test NPS Data")
+//    public void testNPSData() {
+//        List<NationalPark> l = new NPSUtils().getNationalParks();
+//        private String id;
+//        private String description;
+//        private String name;
+//        private Double latitude;
+//        private Double longitude;
+//        private Activity[] activities;
+//        NationalPark target = new NationalPark();
+//        target.setFullName("Abraham Lincoln Birthplace National Historical Park");
+//        assertTrue(l.contains();
+//    }
+}
