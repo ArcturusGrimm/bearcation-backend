35bd76d16dd82531fde7924259fa82492fe9b18d
PatrickBoyle11 <91507912+PatrickBoyle11@users.noreply.github.com>
2022-05-03 00:35:47 -0500

    Added new endpoints
    Added new endpoints

--- src/main/java/bearcation/controller/ReviewController.java
+++ src/main/java/bearcation/controller/ReviewController.java
@@ -2,12 +2,16 @@ package bearcation.controller;
 
 import bearcation.model.dto.LocationDTO;
 import bearcation.model.dto.ReviewDTO;
+import bearcation.model.dto.UserDTO;
+import bearcation.model.entities.Review;
 import bearcation.model.requests.CreateLocationRequest;
 import bearcation.model.requests.CreateReviewRequest;
 import bearcation.service.LocationService;
 import bearcation.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -23,6 +27,22 @@ public class ReviewController {
     public ReviewDTO createReview(@RequestBody CreateReviewRequest createReviewRequest) {
         return reviewService.createReview(createReviewRequest);
     }
+    @GetMapping("/search/location/{id}")
+    public List<ReviewDTO> findReviewsByLocation(@PathVariable Long id){
+        return reviewService.findReviewsByLocation(id);
+    }
+
+    @GetMapping("/search/user/{id}")
+    public List<ReviewDTO> findReviewsByReviewer(@PathVariable Long id){
+        return reviewService.findReviewsByReviewer(id);
+    }
+
+//
+//
+//    @GetMapping("/reviews")
+//    public List<Review> getReviewsByUserId() {
+//        return reviewService.getReviews();
+//    }
 }
 
 //    @Autowired
--- src/main/java/bearcation/repository/ReviewRepository.java
+++ src/main/java/bearcation/repository/ReviewRepository.java
@@ -1,10 +1,22 @@
 package bearcation.repository;
 
+import bearcation.model.entities.Location;
 import bearcation.model.entities.Review;
+import bearcation.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+//    List<Review> findAllByUserId(Iterable<Long> longs);
+//        List<Review> findAllByUser_id(Long id);
+//        findReviewByRating;
+    Optional<List<Review>> findReviewsByReviewer(User u);
+    Optional<List<Review>> findReviewsByLocation(Location l);
+
 
 }
